ce64f8c4b50c0037a12692b1ea6a3915374d5f47
lostje <anze.anzic@siol.net>
2015-05-14 00:09:12 +0200

    Initial commit

---
+++ pacificaairways.js
@@ -0,0 +1,16 @@
+angular.module('PacificaAirways', []);
+angular.module('PacificaAirways').controller('PAController', function($scope) {
+	$scope.totalCosts = 0;
+	$scope.departureAirportsArray = [{ name: 'Ljubljana', price: 100 }, { name: 'Maribor', price: 200}, { name: 'Gradec', price: 300 }];
+	$scope.arrivalAirportsArray = [{ name: 'London Gatwick', price: 100 }, { name: 'London Heathrow', price: 200 }];
+
+	$scope.submitForm = function() {
+		alert("Naročilo uspešno oddano!");
+	}
+	
+	$scope.CalculateCosts = function(departureAirport, arrivalAirport) {
+		if (departureAirport !== undefined && arrivalAirport !== undefined) {
+			$scope.totalCosts = departureAirport.price + arrivalAirport.price;
+		}
+	}
+});
